581bbc7e3e7c03def80cb4e4a9a3851ec63ce0ba
Cortney Cantwell <135674870+cmcantwell@users.noreply.github.com>
2023-10-05 08:27:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -65,13 +65,22 @@ def age_bar():
     return jsonify(results)
 
 
+#@app.route('/cdc_data')
+#def get_cdc_data():
+    #cdc_api_url = 'https://data.cdc.gov/resource/2ew6-ywp6.json?$query=SELECT%20%60wwtp_jurisdiction%60%2C%20%60date_start%60%2C%20%60date_end%60%2C%20%60percentile%60'
+    #response = requests.get(cdc_api_url).json()
+
+    #return jsonify(response)
+    
 @app.route('/cdc_data')
 def get_cdc_data():
-    cdc_api_url = 'https://data.cdc.gov/resource/2ew6-ywp6.json?$query=SELECT%20%60wwtp_jurisdiction%60%2C%20%60date_start%60%2C%20%60date_end%60%2C%20%60percentile%60'
-    response = requests.get(cdc_api_url).json()
+     con = sqlite3.connect("seward.sqlite")
+    cur = con.cursor()
+    query = """
+	SELECT wwtp_jurisdiction, date_start, date_end, percentile FROM 'NWSS_Public_SARS-CoV-2_Wastewater_Metric_Data' 
+	
 
-    return jsonify(response)
-    
+	"""
     
 
 if __name__ == '__main__':

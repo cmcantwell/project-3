a7c7d71d5027b343a5c10a72c7eb9d64baafcf99
jules-krohn <134563265+jules-krohn@users.noreply.github.com>
2023-10-04 22:31:49 -0400

    new bar chart

--- charts.js
+++ charts.js
@@ -1,5 +1,7 @@
 let bar_route_url = "http://127.0.0.1:3009/api/v1.0/covid_bar"
 
+let age_url = "http://127.0.0.1:3009/api/v1.0/age_bar"
+
 let cdc_url = "http://127.0.0.1:3009/cdc_data"
 
 //console.log(bar_route_url)
@@ -14,7 +16,7 @@ for (i = 0; i < data.length; i++) {
 } 
 //console.log(states)
 //console.log(numbers)
-  
+
 var data = [
   {
     x: states,
@@ -22,7 +24,31 @@ var data = [
     type: 'bar'
   }
 ];
-
-Plotly.newPlot('bar', data);
+let layout = {
+        title: "Total Vaccines Administered by State, 2021-2023",
+        legend: False
+}
+Plotly.newPlot('bar', data, layout);
 
 })
+
+d3.json(age_url).then((data) => {
+    let x = ["Age 5 Plus", "Age 12 Plus", "Age 18 Plus", "Age 65 Plus"]
+    let y = data
+    let data1 = [
+        {
+            x: x,
+            y: y,
+            type: 'bar'
+        }
+    ];
+let layout = {
+        title: "Total Vaccines Given by Age Group, 2021 - 2023",
+        legend: False
+}
+Plotly.newPlot('bar', data, layout)
+
+})
+
+
+
